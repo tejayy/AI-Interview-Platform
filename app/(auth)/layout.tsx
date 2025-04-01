187e06165b3aeacc22ce341abd41ab1021120cdc
tejayy <tbj456@gmail.com>
2025-04-01 16:49:14 +0530

    added auth routes

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,8 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React, {ReactNode} from "react";
+
+const AuthLayout = ({children}: {children: ReactNode}) => {
+  return <div>{children}</div>;
+};
+
+export default AuthLayout;
